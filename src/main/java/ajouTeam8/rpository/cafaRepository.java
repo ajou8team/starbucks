5fa03271a2d43ce672ce193137f2429c04ed247a
MOHyeonMin <mhmrm@ajou.ac.kr>
2019-07-20 12:27:00 +0900

    make interface cafaRepository

--- src/main/java/ajouTeam8/rpository/cafaRepository.java
+++ src/main/java/ajouTeam8/rpository/cafaRepository.java
@@ -1,4 +1,12 @@
 package ajouTeam8.rpository;
 
-public class cafaRepository {
+import ajouTeam8.domain.Menu;
+
+import java.util.List;
+
+public interface cafaRepository {
+    List<Menu>  findAll();
+
+    Menu findByName(String name);
+
 }
