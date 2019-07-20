51e4c91a0fa0a39c4eae118902a9918fed91364a
MOHyeonMin <mhmrm@ajou.ac.kr>
2019-07-20 12:57:45 +0900

    make Customer new java class

---
+++ src/main/java/ajouTeam8/domain/Customer.java
@@ -0,0 +1,11 @@
+package ajouTeam8.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Customer {
+    private String customerName;
+    private int number;
+}
