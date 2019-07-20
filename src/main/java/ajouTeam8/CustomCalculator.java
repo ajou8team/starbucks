d6a7917bf840f8d7b6f6a3592d4e2033d44bcf64
MOHyeonMin <mhmrm@ajou.ac.kr>
2019-07-20 12:51:56 +0900

    add the subtract method in CustomCalculator

--- src/main/java/ajouTeam8/CustomCalculator.java
+++ src/main/java/ajouTeam8/CustomCalculator.java
@@ -4,6 +4,10 @@ public class CustomCalculator {
     public int add(int valueNum1, int valueNum2){
         return valueNum1 + valueNum2;
     }
+    public int subtract(int valueNum1, int valueNum2){
+        return valueNum1 - valueNum2;
+
+    }
 
 
 }
